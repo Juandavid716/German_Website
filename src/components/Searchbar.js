c1514cc3f641abe316c84da6a69d66e98a178103
juandavid716 <jbojato@uninorte.edu.co>
2020-05-29 18:25:05 -0500

    substantive subsections added

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,12 +5,30 @@ export const Searchbar = (props) => {
   //const [word, setWord] = useState("");
   // console.log(props.check);
   const [filterDisplay, setFilterDisplay] = useState(props.karte);
-  const [check, setCheck] = useState(false);
-  const [checkDie, setCheckDie] = useState(false);
-  const [checkDas, setCheckDas] = useState(false);
-
+  const [state, setstate] = useState(props.karte);
+  let prueba = [];
+  function change(e) {
+    if (e === "Der") {
+      console.log("HOLA");
+      prueba = props.karte.filter((karte) => karte.color.includes("Blue"));
+      setFilterDisplay(prueba);
+      setstate(prueba);
+    } else if (e === "Die") {
+      prueba = props.karte.filter((karte) => karte.color.includes("Red"));
+      setFilterDisplay(prueba);
+      setstate(prueba);
+    } else if (e === "Das") {
+      prueba = props.karte.filter((karte) => karte.color.includes("Green"));
+      setFilterDisplay(prueba);
+      setstate(prueba);
+    } else if (e === "Alles") {
+      setFilterDisplay(props.karte);
+      setstate(props.karte);
+    }
+    document.getElementById("search-input").value = "";
+  }
   const handleChange = (e) => {
-    let oldList = props.karte.map((karte) => {
+    let oldList = state.map((karte) => {
       return {
         title: karte.title,
         color: karte.color,
@@ -29,94 +47,51 @@ export const Searchbar = (props) => {
           karte.title.toLowerCase().includes(e.toLowerCase()) ||
           karte.traduccion.toLowerCase().includes(e.toLowerCase())
       );
-      if (
-        (check === true && checkDas === true) ||
-        (check === true && checkDie === true) ||
-        (checkDas === true && checkDie === true) ||
-        (check === true && checkDie === true && checkDas === true) ||
-        check === true ||
-        checkDas === true ||
-        checkDie === true
-      ) {
-        let vectorfinal = [];
-        let tmp = [];
-        if (check === true) {
-          tmp = newList.filter((filtro) => filtro.color.includes("Blue"));
-          vectorfinal = vectorfinal.concat(tmp);
-        }
-        if (checkDie === true) {
-          tmp = newList.filter((filtro) => filtro.color.includes("Red"));
-          vectorfinal = vectorfinal.concat(tmp);
-        }
-        if (checkDas === true) {
-          tmp = newList.filter((filtro) => filtro.color.includes("Green"));
-          vectorfinal = vectorfinal.concat(tmp);
-        }
 
-        setFilterDisplay(vectorfinal);
-      } else {
-        setFilterDisplay(newList);
-      }
+      setFilterDisplay(newList);
     } else {
-      console.log("hola");
-      let tmp = [];
-      let oldvaluefinal = [];
-      if (check === true || checkDas === true || checkDas === true) {
-        if (check === true) {
-          tmp = oldList.filter((filtro) => filtro.color.includes("Blue"));
-          oldvaluefinal = oldvaluefinal.concat(tmp);
-        }
-        if (checkDie === true) {
-          tmp = oldList.filter((filtro) => filtro.color.includes("Red"));
-          oldvaluefinal = oldvaluefinal.concat(tmp);
-        }
-        if (checkDas === true) {
-          tmp = oldList.filter((filtro) => filtro.color.includes("Green"));
-          oldvaluefinal = oldvaluefinal.concat(tmp);
-        }
-        if (check === true && checkDas === true && checkDie === true) {
-          oldvaluefinal = oldList;
-        }
-        setFilterDisplay(oldvaluefinal);
-      } else {
-        setFilterDisplay(oldList);
-      }
+      setFilterDisplay(oldList);
     }
   };
 
   return (
     <div className="search-bar container">
+      <div className="botones-op">
+        <button
+          className="button-sus gray"
+          value="Alles"
+          onClick={(e) => change(e.target.value)}
+        >
+          Alles
+        </button>
+        <button
+          className="button-sus Blue"
+          value="Der"
+          onClick={(e) => change(e.target.value)}
+        >
+          Der Articles
+        </button>
+        <button
+          className="button-sus Red"
+          value="Die"
+          onClick={(e) => change(e.target.value)}
+        >
+          Die articles
+        </button>
+        <button
+          className="button-sus Green"
+          value="Das"
+          onClick={(e) => change(e.target.value)}
+        >
+          Das articles
+        </button>
+      </div>
       <input
+        id="search-input"
         onChange={(e) => handleChange(e.target.value)}
         className="myInput"
         placeholder="schreibe ein Substantiv.."
       ></input>
-      <input
-        id="der-art"
-        type="checkbox"
-        onChange={(e) => setCheck(e.target.checked)}
-      ></input>
-      <label htmlFor="der-art" style={{ marginLeft: 8 }}>
-        Der article
-      </label>
-
-      <input
-        id="die-art"
-        type="checkbox"
-        onChange={(e) => setCheckDie(e.target.checked)}
-      ></input>
-      <label htmlFor="die-art" style={{ marginLeft: 8 }}>
-        Die article
-      </label>
-
-      <input
-        id="das-art"
-        type="checkbox"
-        onChange={(e) => setCheckDas(e.target.checked)}
-      ></input>
-      <label htmlFor="das-art" style={{ marginLeft: 8 }}>
-        Das article
-      </label>
 
       <div>
         {" "}
