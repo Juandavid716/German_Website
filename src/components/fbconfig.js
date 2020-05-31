71da6e0a8785925145f9122b22e960368d1201b1
juandavid716 <jbojato@uninorte.edu.co>
2020-05-31 18:24:16 -0500

    solved git ign

---
+++ src/components/fbconfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCx9HEmUqV_aaqiqC6EVa73vWMB9Zk_a_k",
+  authDomain: "imgs-german-site.firebaseapp.com",
+  databaseURL: "https://imgs-german-site.firebaseio.com",
+  projectId: "imgs-german-site",
+  storageBucket: "imgs-german-site.appspot.com",
+  messagingSenderId: "570171886493",
+  appId: "1:570171886493:web:580e228f8c4c726abd7d74",
+  measurementId: "G-3NTB4F3RSG",
+};
