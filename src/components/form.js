1cd882ba36e170caa805488daa1ffeb30b6afd50
juandavid716 <jbojato@uninorte.edu.co>
2020-06-06 15:41:17 -0500

    fix: storage error

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "firebase/auth";
 import "firebase/database";
+import "firebase/storage";
 import firebase from "firebase/app";
 import { useUser } from "reactfire";
 import "./fbconfig";
