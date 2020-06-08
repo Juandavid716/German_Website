3cb95ad77a2a2de3b80acdbaa868626a6c765728
juandavid716 <jbojato@uninorte.edu.co>
2020-06-07 20:58:05 -0500

    feature: visual delete button

--- src/components/box.js
+++ src/components/box.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import '../css/box.css';
-
+import React from "react";
+import "../css/box.css";
+import { useUser } from "reactfire";
 function Box(props) {
+  const user = useUser();
   return (
     <div className="box-father">
       <div className="box">
         <div className={`box-text ${props.color}`}>
-          <h1 className="Title"> {props.title}</h1>
+          <h1> {props.title}</h1>
+
+          {user && (
+            <div className="delete-button">
+              <span className="material-icons btn-del">delete</span>
+            </div>
+          )}
           <p>
             <em> {props.text}</em>
           </p>
